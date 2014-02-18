152ac4938c3d64eeffe3f652122bb49d016de82d
Peter Cass <peter@casswitz.com>
2014-02-18 10:40:09 -0500

    Gettings the forms in shape w/ helper Adding connection constraints

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,13 @@
 class Wine < ActiveRecord::Base
   belongs_to :brand
   belongs_to :variety
-  has_many :bottles, :dependent => :destroy, :through => wines
+  has_many :bottles, :dependent => :destroy
+  has_many :stores, :through => :bottles, :dependent => :destroy
+
+  #add_index :categories_posts, [ :category_id, :post_id ], :unique => true, :name => 'by_category_and_post'
+
+  def wine_name
+    [ brand.name, variety.name ].reject(&:blank?).join(':')
+  end
 
 end
