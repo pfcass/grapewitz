ae470ad297ecd3a4b3776ed56791df5b7d16336d
Peter Cass <peter@casswitz.com>
2014-03-07 14:24:38 +0000

    Add index screen of all users Text on initial screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :signup_failed
+
+
   def index
+    @users = User.all
   end
 
   def new
@@ -7,9 +11,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(email: params[:user][:email],
-                password: params[:user][:password],
-                password_confirmation: params[:user][:password_confirmation])
+    respond_to do |format|
+      if user = User.create!(email: params[:user][:email],
+                      password: params[:user][:password],
+                      password_confirmation: params[:user][:password_confirmation])
+
+        format.html { redirect_to users_index_path, notice: 'Wino was successfully created.' }
+        format.json { render action: 'show', status: :created, location: user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -24,4 +37,14 @@ class UsersController < ApplicationController
   def show
     @bottles = Bottle.find_all_by_user_id( current_user.id )
   end
+
+
+
+  private
+
+  def signup_failed
+    flash[:error] = "Creation Failed"
+    redirect_to users_index_path, notice: 'Wino creation failed'
+  end
+
 end
