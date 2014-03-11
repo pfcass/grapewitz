014cd21d7b463691a018b8cad93f85c10d8593ea
Peter Cass <peter@casswitz.com>
2014-03-11 15:59:31 +0000

    page formatting and linkage

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -5,6 +5,7 @@ class BottlesController < ApplicationController
   # GET /bottles.json
   def index
     @bottles = Bottle.all
+    @id = params[:user_id] == nil ? -1 : params[:user_id].to_i
   end
 
   # GET /bottles/1
@@ -23,6 +24,14 @@ class BottlesController < ApplicationController
 
   # GET /bottles/1/edit
   def edit
+    this_bottle = Bottle.find(params[:id].to_i)
+    bottles = Bottle.where( "wine_id = ?", this_bottle.wine_id )
+    @users = []
+    bottles.each do |b|
+      if b.user_id != this_bottle.user_id
+        @users.push( User.find( b.user_id ) )
+      end
+    end
   end
 
   # POST /bottles
@@ -32,7 +41,7 @@ class BottlesController < ApplicationController
     @bottle.user_id = current_user.id
     respond_to do |format|
       if @bottle.save
-        format.html { redirect_to @bottle, notice: 'Bottle was successfully created.' }
+        format.html { redirect_to users_show_path(current_user.id), notice: 'Bottle was successfully created.' }
         format.json { render action: 'show', status: :created, location: @bottle }
       else
         format.html { render action: 'new' }
