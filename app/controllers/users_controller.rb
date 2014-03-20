55c1e7381e3ffbbac20dba97769c0c64210d1357
Peter Cass <pfcass@casswitz.com>
2014-03-20 12:22:06 +0000

    Add price stats to MY CELLAR

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,33 @@ class UsersController < ApplicationController
     end
     @greeting = User.find(user_id).greeting
     @bottles = Bottle.where("user_id = #{user_id}")
+
+    # We're going to compute some extra info here
+    #
+    @n_bottles = 0
+    @total_paid_price = 0.0
+    @total_list_price = 0.0
+    @total_saved = 0.0
+    @bottles.each do |b|
+      if b.price == nil && b.list_price == nil
+        # no price info, so skip it
+        next
+      elsif b.price != nil && b.list_price != nil
+        # have paid and list, so we can do a full analysis
+        @total_paid_price += b.price * b.quantity
+        @total_list_price += b.list_price * b.quantity
+        @total_saved += (b.list_price - b.price) * b.quantity
+      elsif b.price != nil
+        # go w/ the paid price
+        @total_paid_price += b.price * b.quantity
+        @total_list_price += b.price * b.quantity
+      elsif b.list_price != nil
+        #user the list price
+        @total_paid_price += b.list_price * b.quantity
+        @total_list_price += b.list_price * b.quantity
+      end
+      @n_bottles += b.quantity
+    end
   end
 
 
