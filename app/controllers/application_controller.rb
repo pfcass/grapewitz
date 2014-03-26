c4276c20706c17e3b01e7c1e1d2d416a73f0c28b
Peter Cass <pfcass@casswitz.com>
2014-03-26 11:29:04 +0000

    Add user_name field to create/edit form for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user!
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :user_name
+    devise_parameter_sanitizer.for(:account_update) << :user_name
+  end
+
 end
