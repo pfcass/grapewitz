19a412c7034b146105701e83321a299b0b46754a
Peter Cass <pfcass@casswitz.com>
2014-03-26 10:30:20 +0000

    enable registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable  #, :registerable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
 
   has_many :bottles, dependent: :destroy
   has_many :links, dependent: :destroy
